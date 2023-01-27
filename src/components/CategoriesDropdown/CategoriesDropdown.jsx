451c0e6da099f6b9712c35582fb3b050f9551fcb
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2023-01-27 22:49:02 +0600

    dropdown select added

--- src/components/CategoriesDropdown/CategoriesDropdown.jsx
+++ src/components/CategoriesDropdown/CategoriesDropdown.jsx
@@ -33,7 +33,7 @@ const CategoriesDropdown = ({ idText }) => {
           <Loader></Loader>
         ) : (
           categories?.map((category, idx) => (
-            <div key={category._id} className="accordion px-5">
+            <div key={category?._id} className="accordion px-5">
               <input
                 className="drop-input"
                 type="checkbox"
@@ -46,17 +46,18 @@ const CategoriesDropdown = ({ idText }) => {
                 htmlFor={`${idText}${idx + 1}`}
               >
                 <img src={category.image} alt="" className="w-4 h-4" />
-                {category.categoryName}
+                {category?.categoryName}
               </label>
 
               <div className="accordion-content flex flex-col">
-                {category.subCategories.map((tag, idx) => (
+                {category?.subCategories?.map((subCategory, idx) => (
                   <Link
+                    to={`/category/${category?._id}?subCat=${subCategory}`}
                     className="text-black/80 flex items-center gap-x-2 tori-link"
                     key={idx}
                   >
                     <AiOutlineRight />
-                    {tag}
+                    {subCategory}
                   </Link>
                 ))}
               </div>
