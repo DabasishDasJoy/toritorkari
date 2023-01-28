a41901a34ba149deb9841380fe4c17f61e1acb30
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2023-01-28 19:54:29 +0600

    refactor categories route path

--- src/components/CategoryCardMinified/CategoryCardMinified.jsx
+++ src/components/CategoryCardMinified/CategoryCardMinified.jsx
@@ -5,7 +5,7 @@ const CategoryCardMinified = ({ category: { categoryName, _id, image } }) => {
   const navigate = useNavigate();
   return (
     <div
-      onClick={() => navigate(`/category/${_id}?subCat=All`)}
+      onClick={() => navigate(`/category/${_id}?subcategory=all`)}
       className="w-24 hover:shadow-lg cursor-pointer flex flex-col gap-1 justify-center items-center h-auto p-2 lg:my-2 bg-white rounded-md text-black"
     >
       <div className="p-2 flex items-center justify-center shadow-lg rounded-full">
