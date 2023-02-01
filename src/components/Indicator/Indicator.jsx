c39003bfd564c836b30c9a58b6f4d4c19c4678ce
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2023-02-02 00:11:35 +0600

    refactor indicator

--- src/components/Indicator/Indicator.jsx
+++ src/components/Indicator/Indicator.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Indicator = ({ children }) => {
   return (
     <span className="badge badge-sm text-white indicator-item badge-warning p-1">
-      {children}
+      {children ? children : 0}
     </span>
   );
 };
