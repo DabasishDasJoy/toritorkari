6c131670c17adc43d59b06d14b379165aff593f2
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2023-02-09 15:06:42 +0600

    added map in contact

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
-import ComingSoon from "../../components/ComingSoon/ComingSoon";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+import "./ContactUs.css";
 
 const ContactUs = () => {
+  const position = [51.505, -0.09];
+
   return (
     <div>
       <Helmet>
@@ -12,7 +16,22 @@ const ContactUs = () => {
           content="Contact information of the authority."
         />
       </Helmet>
-      <ComingSoon></ComingSoon>
+
+      {/* Map */}
+      <div>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>{position}</Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+
+      {/* Contact form */}
+      <div></div>
     </div>
   );
 };
