bc50a3c1ec11840f548fd7b0b300a94565a30a08
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2023-02-11 12:13:07 +0600

    refactor logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/logo.png";
 const Logo = () => {
   return (
     <div className="flex gap-1 items-center">
-      <img src={logo} alt="" className="w-10" />
+      <img src={logo} alt="" className="w-7" />
       <div className="flex-col items-start flex ">
         <span className="text-xl leading-none font-bold">ToriTorkari</span>
         <small className="text-xs leading-none">Find Your Green Here</small>
